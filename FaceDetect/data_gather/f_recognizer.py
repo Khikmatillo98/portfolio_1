b7fd13a40e23099e0002b533974a4cde767b2c33
Khikmatillo98 <70311209+Khikmatillo98@users.noreply.github.com>
2020-10-17 12:40:38 +0500

    Update f_recognizer.py

--- FaceDetect/data_gather/f_recognizer.py
+++ FaceDetect/data_gather/f_recognizer.py
@@ -13,7 +13,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX ##################
 id = 0
 
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['Asror akam','Tillo', 'T', 'Zaynab', 'Zaynabxon'] 
+names = ['A','B'] 
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
